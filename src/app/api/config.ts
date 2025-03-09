c589b174c1ae1de52b863b841b04afef53fca6e5
KobayaClanguage <kobayasiharuto@gmail.com>
2025-03-09 19:10:18 +0900

    apiのルーティングを変更し、ログインシステムのapiを作成 #5

---
+++ src/app/api/config.ts
@@ -0,0 +1,11 @@
+import admin from "firebase-admin";
+
+// Firebase Admin SDK の初期化
+if (!admin.apps.length) {
+    const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS || '{}');
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+}
+
+export let adminAuth = admin.auth();
