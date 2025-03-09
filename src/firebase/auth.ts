bf7f0343507c023b13645c6a85e9800427447a76
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2025-03-09 17:11:33 +0900

    add : firebase settings

---
+++ src/firebase/auth.ts
@@ -0,0 +1,50 @@
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import app from "@/firebase/config";
+
+const auth = getAuth(app);
+
+// サインアップ
+export const signUp = async (email: string, password: string) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    return userCredential.user;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// ログイン(サインイン)
+export const signIn = async (email: string, password: string) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    return userCredential.user;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// ログアウト
+export const logout = async () => {
+  const auth = getAuth(app);
+  try {
+    await signOut(auth);
+    console.log("User signed out successfully.");
+  } catch (error) {
+    console.error("Error signing out:", error);
+  }
+};
