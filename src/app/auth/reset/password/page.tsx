3d0dbd9d75a198a356de5c49e2386d775053e21d
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2025-04-07 17:39:32 +0900

    update : タグを修正

--- src/app/auth/reset/password/page.tsx
+++ src/app/auth/reset/password/page.tsx
@@ -32,8 +32,8 @@ export default function AuthResetPassword() {
         />
       </div>
       <div className="mb-4 mt-10 flex flex-col items-center space-y-2">
-        <h2 className="text-2xl font-bold">オブジェを探せゲーム</h2>
-        <p className="text-2xl text-gray-700">パスワード再設定</p>
+        <h1 className="text-2xl font-bold">オブジェを探せゲーム</h1>
+        <h2 className="text-2xl text-gray-700">パスワード再設定</h2>
       </div>
       <div className="flex flex-col items-center justify-around space-y-6 px-10">
         <p className="px-5 text-xl">
