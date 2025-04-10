9fa58a2984a45cbff7d58ac4d6b6cc95d061ddd0
KobayaClanguage <kobayasiharuto@gmail.com>
2025-04-10 12:27:05 +0900

    メールアドレス変更完了画面を実装した

---
+++ src/app/game/settings/account/change/email/complete/page.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import NavigationFooter from "@/features/game/NavigationFooter";
+import React from "react";
+
+export default function GameSettingsAccountChangePasswordComplete() {
+  const pageTitle = "設定";
+  const pageSubTitle = "メールアドレス変更";
+
+  return (
+    <div className="relative h-full">
+      <h1 className="fixed inset-x-0 top-0 bg-[#0094f4] p-4 pt-7 text-center text-3xl text-white">
+        {pageTitle}
+      </h1>
+      <div className="pb-16 pt-20">
+        <div className="relative mx-6 flex h-[80px] items-center justify-around">
+          <h1 className="text-2xl">{pageSubTitle}</h1>
+        </div>
+        {/* (画面サイズの縦幅:100vh) - (タイトルバーの縦幅:80px) - (ナビゲーションバーの縦幅:74px) - (スタンプ名タイトルの縦幅:80px) */}
+        <div className="relative mt-4 flex h-[calc(100vh-80px-74px-80px)] w-full flex-col items-center justify-start px-9 text-xl">
+          <p>メールアドレスの変更が完了しました。</p>
+          <p>ログインし直してください。</p>
+          <Button
+            asChild
+            className="mb-4 mt-9 h-14 w-full rounded-none bg-[#0094f4] text-2xl"
+          >
+            <Link href="/auth/login">ログイン画面に戻る</Link>
+          </Button>
+        </div>
+      </div>
+      <div className="fixed inset-x-0 bottom-0 flex items-center justify-around border bg-white p-4 shadow-md">
+        <NavigationFooter />
+      </div>
+    </div>
+  );
+}
