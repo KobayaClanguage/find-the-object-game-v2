f8ffbc867f1de79d60cde3f102c3d9944545addb
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2025-04-24 12:42:46 +0900

    add : VercelのDeploy Failedを避けるために空のファイルを追加

---
+++ src/app/game/scan/page.tsx
@@ -0,0 +1,8 @@
+export default function GameScan() {
+  // TODO: フロントエンド実装
+  return (
+    <>
+      <h1>GameScanページ</h1>
+    </>
+  );
+}
---
+++ src/app/game/settings/info/howToPlay/page.tsx
@@ -0,0 +1,8 @@
+export default function GameSettingsInfoHowToPlay() {
+  // TODO: フロントエンド実装
+  return (
+    <>
+      <h1>HowToPlayページ</h1>
+    </>
+  );
+}
