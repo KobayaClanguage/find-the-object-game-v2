e83d68c203a7448bb8a5c438330d7b2ba2607e97
KobayaClanguage <kobayasiharuto@gmail.com>
2025-04-30 15:49:54 +0900

    アカウント削除関数を実装した

--- src/features/auth/auth.ts
+++ src/features/auth/auth.ts
@@ -1,8 +1,11 @@
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  deleteUser,
+  reauthenticateWithCredential
 } from "firebase/auth";
 import { auth } from "@/firebase/config";
+import { EmailAuthProvider } from "firebase/auth/web-extension";
 
 export async function signupWithEmail(email: string, password: string) {
   try {
@@ -20,5 +23,28 @@ export async function signinWithEmail(email: string, password: string) {
     return { success: true };
   } catch {
     return { success: false, error_message: "ログインに失敗しました" };
+  }
+}
+
+export async function deleteAccount(password: string) {
+  try {
+    const user = auth.currentUser;
+    const email = auth.currentUser?.email;
+
+    if(user === null || email === null || email === undefined) {
+      return { success: false, error_message: "アカウント削除に失敗しました"};
+    } 
+
+    const credential = EmailAuthProvider.credential(
+      email,
+      password
+    )
+
+    await reauthenticateWithCredential(user, credential);
+    await deleteUser(user);
+
+    return { success: true};
+  } catch {
+    return { success: false, error_message: "アカウント削除に失敗しました"};
   }
 }
