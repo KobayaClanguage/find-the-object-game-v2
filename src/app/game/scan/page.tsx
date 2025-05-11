6b5c44f16f546ef025d9850cd9d5bc01272cdad6
KobayaClanguage <kobayasiharuto@gmail.com>
2025-05-11 13:32:25 +0900

    カメラ映像の中央寄せが失敗していたので戻す

--- src/app/game/scan/page.tsx
+++ src/app/game/scan/page.tsx
@@ -19,30 +19,14 @@ export default function GameScan() {
       <h1 className="fixed inset-x-0 top-0 bg-[#0094f4] p-4 pt-7 text-center text-3xl text-white">
         {pageTitle}
       </h1>
-      <div className="min-h-screen flex flex-col">
-        <header className="text-center pt-4">
-          <h1>Find-the-object-game</h1>
-        </header>
-
-        <main className="flex-grow flex items-center justify-center relative">
-          <video
-            id="video"
-            autoPlay
-            muted
-            playsInline
-            className="max-w-full max-h-full object-contain"
-          ></video>
-          <canvas
-            className="hidden"
-            id="camera-canvas"
-            ref={canvasRef}
-          ></canvas>
-          <canvas id="react-canvas" className="absolute top-0 left-0"></canvas>
-        </main>
-
-        <footer className="fixed inset-x-0 bottom-0 flex items-center justify-around border bg-white p-4 shadow-md">
-          <NavigationFooter />
-        </footer>
+      <div>
+        <h1>Find-the-object-game</h1>
+        <video className="" id="video" autoPlay muted playsInline></video>
+        <canvas className="hidden" id="camera-canvas" ref={canvasRef}></canvas>
+        <canvas id="react-canvas"></canvas>
+      </div>
+      <div className="fixed inset-x-0 bottom-0 flex items-center justify-around border bg-white p-4 shadow-md">
+        <NavigationFooter />
       </div>
     </div>
   );
