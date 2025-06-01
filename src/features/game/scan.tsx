d41c66e1656d10b90ae685d7cbdd073ab8f56da9
KobayaClanguage <kobayasiharuto@gmail.com>
2025-06-01 15:16:32 +0900

    外カメラを使用するように変更した

--- src/features/game/scan.tsx
+++ src/features/game/scan.tsx
@@ -20,7 +20,7 @@ export async function Scan(
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: false,
-      video: { width: 640, height: 480 },
+      video: { width: 640, height: 480, facingMode: { exact: "environment" } },
     });
 
     video.srcObject = stream;
