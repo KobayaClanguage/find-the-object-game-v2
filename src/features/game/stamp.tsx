1d772db14718a4c26d68b803230b7d3faea27765
KobayaClanguage <kobayasiharuto@gmail.com>
2025-06-27 19:07:17 +0900

    Revert "間違ってmainブランチにpushしてしまったので、revertする"
    This reverts commit 04d00de7668e0bb3d4ef9ddf546ca4f8b7ee0af6.

--- src/features/game/stamp.tsx
+++ src/features/game/stamp.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "@/firebase/config";
-import { fieldName, stampName } from "@/features/game/stampData";
 
 export type StampInfo = {
   id: number;
@@ -11,6 +10,31 @@ export type StampInfo = {
 };
 
 export async function fetchStamps(uid: string) {
+  const fieldName = [
+    "object_1",
+    "object_2",
+    "object_3",
+    "object_4",
+    "object_5",
+    "object_6",
+    "object_7",
+    "object_8",
+    "object_9",
+    "object_10",
+  ];
+  const stampName = [
+    "入口",
+    "階段",
+    "駐車場",
+    "ベンチ",
+    "すべり台",
+    "時計",
+    "Undefined1",
+    "Undefined2",
+    "Undefined3",
+    "Undefined4",
+  ];
+
   const docRef = doc(db, "game_progress", uid);
   const docSnap = await getDoc(docRef);
 
