04d00de7668e0bb3d4ef9ddf546ca4f8b7ee0af6
KobayaClanguage <kobayasiharuto@gmail.com>
2025-06-27 19:02:07 +0900

    スタンプ情報を別ファイルに分割した

---
+++ src/features/game/stampData.ts
@@ -0,0 +1,25 @@
+export const fieldName = [
+  "object_1",
+  "object_2",
+  "object_3",
+  "object_4",
+  "object_5",
+  "object_6",
+  "object_7",
+  "object_8",
+  "object_9",
+  "object_10",
+];
+
+export const stampName = [
+  "入口",
+  "階段",
+  "駐車場",
+  "ベンチ",
+  "すべり台",
+  "時計",
+  "Undefined1",
+  "Undefined2",
+  "Undefined3",
+  "Undefined4",
+];
