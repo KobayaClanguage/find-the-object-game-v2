ef59ad23c098446f4bfad5df7ab38e5fdc716a18
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2025-07-04 00:19:20 +0900

    update : aria-labelを追加した. アクセシビリティー向上のため

--- src/features/game/NavigationFooter.tsx
+++ src/features/game/NavigationFooter.tsx
@@ -5,13 +5,13 @@ export default function NavigationFooter() {
   const buttonSize = { x: 40, y: 40 };
   return (
     <>
-      <Link href="/game/scan">
+      <Link href="/game/scan" aria-label="Scan QR Code">
         <ScanQrCode width={buttonSize.x} height={buttonSize.y} />
       </Link>
-      <Link href="/game/stamp">
+      <Link href="/game/stamp" aria-label="Home">
         <House width={buttonSize.x} height={buttonSize.y} />
       </Link>
-      <Link href="/game/settings">
+      <Link href="/game/settings" aria-label="Settings">
         <Settings width={buttonSize.x} height={buttonSize.y} />
       </Link>
     </>
