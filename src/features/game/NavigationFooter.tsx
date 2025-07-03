61e59125f30a69e28e7e39e2a88d6de6e3cd98c3
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2025-07-04 00:13:28 +0900

    add : ナビゲーションバーのボタンをlucide-reactのアイコンに修正した

--- src/features/game/NavigationFooter.tsx
+++ src/features/game/NavigationFooter.tsx
@@ -1,33 +1,18 @@
 import Link from "next/link";
-import Image from "next/image";
+import { ScanQrCode, House, Settings } from "lucide-react";
 
 export default function NavigationFooter() {
   const buttonSize = { x: 40, y: 40 };
   return (
     <>
       <Link href="/game/scan">
-        <Image
-          src={"/game/navigationBarIcons/scan.png"}
-          alt="home"
-          width={buttonSize.x}
-          height={buttonSize.y}
-        />
+        <ScanQrCode width={buttonSize.x} height={buttonSize.y} />
       </Link>
       <Link href="/game/stamp">
-        <Image
-          src={"/game/navigationBarIcons/home.png"}
-          alt="home"
-          width={buttonSize.x}
-          height={buttonSize.y}
-        />
+        <House width={buttonSize.x} height={buttonSize.y} />
       </Link>
       <Link href="/game/settings">
-        <Image
-          src={"/game/navigationBarIcons/settings.png"}
-          alt="home"
-          width={buttonSize.x}
-          height={buttonSize.y}
-        />
+        <Settings width={buttonSize.x} height={buttonSize.y} />
       </Link>
     </>
   );
