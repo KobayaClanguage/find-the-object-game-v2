28d0b1ad918b560e994736ce51b7d8dfac3ab2c5
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2025-08-22 22:45:13 +0900

    feat: 🎸 準備中メッセージをトップページに追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
   return (
     <main className="my-20 flex min-h-screen flex-col items-center bg-white p-6">
       {/* 上部タイトル */}
-      <h1 className="mb-12 text-4xl font-bold text-gray-800">
+      <h1 className="mb-12 text-3xl font-bold text-gray-800">
         オブジェを探せゲーム
       </h1>
 
       {/* ロゴセクション */}
-      <div className="mb-12 flex flex-col items-center">
+      <div className="mb-4 flex flex-col items-center">
         <div className="mb-6 flex flex-col items-center space-y-2 text-center">
           <Image
             src={"/images/commentLogo.png"}
@@ -36,12 +36,23 @@ export default function Home() {
         </div>
       </div>
 
+      {/* TODO: 準備中メッセージ削除 */}
+      <div className="text-center text-3xl text-red-500">
+        <strong>
+          準備中です
+          <br />
+          来春リリース予定
+        </strong>
+      </div>
+
       {/* サブタイトル */}
-      <h2 className="mb-16 text-center text-3xl font-bold text-gray-800">
-        スタンプを集めて
-        <br />
-        額地区を探索しよう！
-      </h2>
+      <div className="mb-8 mt-4 flex flex-col items-center">
+        <h2 className="mb-16 text-center text-3xl font-bold text-gray-800">
+          スタンプを集めて
+          <br />
+          額地区を探索しよう！
+        </h2>
+      </div>
 
       {/* ボタンセクション */}
       <div className="flex w-full max-w-xs flex-col gap-4">
