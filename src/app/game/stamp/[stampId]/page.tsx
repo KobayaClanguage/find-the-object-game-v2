c0052c55620fe2ce45e7d93594eff84b60f99489
KobayaClanguage <kobayasiharuto@gmail.com>
2025-10-17 17:35:29 +0900

    map画面が表示されるようにした

--- src/app/game/stamp/[stampId]/page.tsx
+++ src/app/game/stamp/[stampId]/page.tsx
@@ -1,18 +1,37 @@
 "use client";
 import { ChevronLeft } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { AuthGuard } from "@/features/auth/authGuard";
 import NavigationFooter from "@/features/game/NavigationFooter";
-
-// import { useSearchParams } from "next/navigation";
+import { doc, getDoc} from "firebase/firestore";
+import { db } from "@/firebase/config";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function GameStampId() {
   const pageTitle = "マップ";
-  // TODO: ページタイトルを動的に変更する
-  // const stampId = useSearchParams().get("stampId");
-  const stampId = "1";
-  const stampName = "駐車場";
+  const params = useParams();
+  const ID = String(params.stampId);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [stampName, setStampName] = useState();
+  const [MapURL, setMapURL] = useState();
+
+  useEffect(() => {
+    const GetObjectInfo = async() => {
+      const NameDocRef = doc(db, "ObjectInfo", ID);
+      try {
+        const NameDocSnap = await getDoc(NameDocRef);
+        setStampName(NameDocSnap.data()?.["Name"]);
+        setMapURL(NameDocSnap.data()?.["MapURL"]);
+      } catch {
+        setErrorMessage("通信エラーが発生しました");
+      }
+    }
+    GetObjectInfo();
+
+  }, [ID]);
+
 
   return (
     <AuthGuard>
@@ -31,11 +50,16 @@ export default function GameStampId() {
           </div>
           {/* (画面サイズの縦幅:100vh) - (タイトルバーの縦幅:80px) - (ナビゲーションバーの縦幅:74px) - (スタンプ名タイトルの縦幅:80px) */}
           <div className="relative h-[calc(100vh-80px-74px-80px)] w-full">
-            <Image
-              src={`/game/stamp/maps/map${stampId}.png`}
-              fill
-              alt={`${stampName} map`}
-            />
+            <div className="text-center text-lg font-bold text-red-500">{errorMessage}</div>
+            <iframe
+              src={MapURL}
+              style={{ border: 0 }}
+              className="size-full"
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            >
+            </iframe>
           </div>
         </div>
         <div className="fixed inset-x-0 bottom-0 flex items-center justify-around border bg-white p-4 shadow-md">
