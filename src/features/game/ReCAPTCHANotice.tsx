9c18a9080ef8f33b79103fa3a483b8b40d966ae8
KobayaClanguage <kobayasiharuto@gmail.com>
2025-10-20 10:50:52 +0900

    lauout.tsxにreCAPTCHAを導入していることを示すテキストを追加した

---
+++ src/features/game/ReCAPTCHANotice.tsx
@@ -0,0 +1,9 @@
+export default function ReCAPTCHANotice() {
+  return (
+    <div className="mb-20 text-center">
+      This site is protected by reCAPTCHA and the Google
+      <a href="https://policies.google.com/privacy">Privacy Policy</a> and
+      <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+    </div>
+  )
+}
